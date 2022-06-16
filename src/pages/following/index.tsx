e0e9b51ed38be601649ac83f2b392a6eb3cad698
Anish42Borkar <74052865+Anish42Borkar@users.noreply.github.com>
2022-06-16 12:08:58 +0530

    implimented basic following pages (#8)
    * Implemented infinity scroll, made components responsive and added MUI icon package
    
    * completed ui of navbar, added topics to
    navbar with smooth scrolling optimize for sm-devices
    
    * alignment changes to topic bar inside navbar
    
    * made infinity scroll smoother
    
    * done topics module with infinite scroll
    
    * design the search layout
    
    * added random wallpaper on hero, fix the
    search box layout issue
    
    * implimented basic following pages

--- src/pages/following/index.tsx
+++ src/pages/following/index.tsx
@@ -4,7 +4,18 @@ import Hero from "../../components/hero";
 import MasonryLayout from "../../components/masonry_layout";
 
 const Following = () => {
-  return <div>Following</div>;
+  return (
+    <div className="mt-7 ps-5">
+      <p className="fs-1 fw-bold">Following</p>
+      <p className="fw-semibold fs-5">
+        The latest photos from photographers you follow.
+      </p>
+      <p className="mt-8 fs-6">
+        No photos to display. Follow some Unsplash photographers and check back
+        soon.
+      </p>
+    </div>
+  );
 };
 
 export default Following;
