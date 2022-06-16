17e88521375577ec3549e0c7d52345040b03ce2b
Anish42Borkar <74052865+Anish42Borkar@users.noreply.github.com>
2022-06-16 21:12:24 +0530

    completed search module with infinity scroll (#9)
    * Implemented infinity scroll, made components responsive and added MUI icon package
    
    * completed ui of navbar, added topics to
    navbar with smooth scrolling optimize for sm-devices
    
    * alignment changes to topic bar inside navbar
    
    * made infinity scroll smoother
    
    * done topics module with infinite scroll
    
    * design the search layout
    
    * added random wallpaper on hero, fix the
    search box layout issue
    
    * implimented basic following pages
    
    * completed search module with infinity scroll

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home";
 import Following from "../pages/following";
 import Topics from "../pages/topics";
+import Search from "../pages/search";
 
 const Routers = (): JSX.Element => {
   return (
@@ -11,6 +12,7 @@ const Routers = (): JSX.Element => {
         <Route path="/" element={<Home />} />
         <Route path="following" element={<Following />} />
         <Route path="topics/:id" element={<Topics />} />
+        <Route path="Search" element={<Search />} />
       </Routes>
     </>
   );
