ac7d67fd3fbd88d032c56fd38966bcab50569c61
Anish42Borkar <74052865+Anish42Borkar@users.noreply.github.com>
2022-06-19 13:38:08 +0530

    added offcanvas (#17)
    * Implemented infinity scroll, made components responsive and added MUI icon package
    
    * completed ui of navbar, added topics to
    navbar with smooth scrolling optimize for sm-devices
    
    * alignment changes to topic bar inside navbar
    
    * made infinity scroll smoother
    
    * done topics module with infinite scroll
    
    * design the search layout
    
    * added random wallpaper on hero, fix the
    search box layout issue
    
    * implimented basic following pages
    
    * completed search module with infinity scroll
    
    * implimented on click preview image functionality
    
    * implimented on click preview with
    previous and next button
    
    * created env file to store api key, made 4
    column max  masonry grid layout
    
    * remove unwanted consoles and code
    
    * completed previus,next button with
    functionality, added overlay
    when hover icons and implimented download image functionality
    
    * added like button partially functional,
    implimented zoom in, out
    functionality and implimented download feature
    
    * added favicon
    
    * added offcanvas

--- src/components/spinner.tsx
+++ src/components/spinner.tsx
@@ -4,7 +4,7 @@ const Spinner = forwardRef((props: any, ref: any): JSX.Element => {
   return (
     <div ref={ref} className="text-center">
       <div className="spinner-border" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only"></span>
       </div>
     </div>
   );
